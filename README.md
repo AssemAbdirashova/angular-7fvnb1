b08b31afe74a577d23294cfc5c7c414abb6c115e
AssemAbdirashova <asemsuper2001@mail.ru>
2020-02-14 12:59:25 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7fvnb1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7fvnb1)
---
+++ src/app/products.ts
@@ -0,0 +1,80 @@
+export const products = [
+  {
+    name: 'Магнитный usb-кабель Twitch Micro USB для Samsung S7, Xiaomi, Microusb, Android, Магнитный зарядный кабель для мобильного телефона, зарядное устройство Micro USB',
+    rating: '4.9/5',
+    description: 'Бренд: TwitchРазъём B: Micro-USBМаксимальный Ток: 2.4AМатериал оплётки: НейлонРазъём A: USB Type-AОсобенности: МагнитныйОсобенности: СворачиваемыйОсобенности: Со светодиодным индикаторомОсобенности: Литые разъёмыВ розничной упаковке: ДаFeature 1: Micro USB Cable / USB Cable Micro USBFeature 2: Magnetic Cable Micro USB CableFeature 3: Android Cable / Microusb Cable',
+    image: 'https://ae01.alicdn.com/kf/H33a76d66fbb44fd5a727d917dfe01a86E.jpg',
+    ref: 'https://aliexpress.ru/item/4000645094554.html?spm=a2g0o.home.15002.8.17ba1b86NTTn8I&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=72efcc0a-35d4-4e88-b77c-f358d54750fe&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:72efcc0a-35d4-4e88-b77c-f358d54750fe,tpp_buckets:668%230%23131923%2318_668%23808%234094%23226_668%23888%233325%235'
+  },
+  {
+    name: 'WEIDE, Новое поступление, модные повседневные кварцевые часы для мужчин, водонепроницаемые, календарь, китайский дракон, ремешок из натуральной кожи, relogio masculino',
+    rating: '5.0/5',
+    description: 'Модель (UV1509) с 7 привлекательными вариантами; -Японский кварцевый механизм MIYOTA 2115;Крутой и специально разработанный большой корпус часов;-Ремешок из натуральной кожи и Пряжка из нержавеющей стали; -Уникальный гальванический 3D логотип WEIDE & Universe на два кольца с ремешком. -Экологический IP черный вакуумный гальванический корпус, и пройти стандарт ROHS; -Корона часов с экологичным ips-бронзовым гальваническим покрытием; -Водонепроницаемость 30 метров (не подходит для плавания, дайвинга, душа и т. д., пожалуйста, не нажимайте кнопки под водой); -Погрешность при ходьбе ± 45/м.',
+    image: 'https://ae01.alicdn.com/kf/HTB18vFAXWagSKJjy0Fgq6ARqFXak/WEIDE.jpg_50x50.jpg',
+    ref:'https://aliexpress.ru/item/32830541186.html?spm=a2g0o.detail.1000014.17.69dd618fbvwvU7&gps-id=pcDetailBottomMoreOtherSeller&scm=1007.14976.147898.0&scm_id=1007.14976.147898.0&scm-url=1007.14976.147898.0&pvid=0388dfcd-328a-49dc-870a-db1b5b93a54d&_t=gps-id:pcDetailBottomMoreOtherSeller,scm-url:1007.14976.147898.0,pvid:0388dfcd-328a-49dc-870a-db1b5b93a54d,tpp_buckets:668%230%23131923%2318_668%23808%234094%23529_668%23888%233325%234'
+  },
+  {
+    name: 'Беспроводные наушники, 3000 мАч, зарядная коробка IPX7, водонепроницаемые TWS наушники 9D, стерео, беспроводные Bluetooth 5,0 наушники с микрофоном',
+    rating: '5.0/5',
+    description: 'Беспроводные наушники, 3000 мАч, зарядная коробка IPX7, водонепроницаемые TWS наушники 9D, стерео, беспроводные Bluetooth 5,0 наушники с микрофоном Название: беспроводные bluetooth-наушники TWS Особенность 1: Bluetooth V5.0, 3000 мАч зарядный чехол, сенсорное управление Особенность 2: наушники Bluetooth 5,0, беспроводная bluetooth-гарнитура Функция: Автоматическое Сопряжение, наушники для iOS Android Phone Емкость корпуса для зарядки: 3000 мАч Время работы: около 4-5 часов 8',
+    image: 'https://ae01.alicdn.com/kf/Hc8f9ee21dc1f4d96b4ca2a604487c4f6Z/3000-IPX7.jpg',
+    ref: 'https://aliexpress.ru/item/4000671108909.html?spm=a2g0o.detail.1000014.13.6b4316c8h8RxFf&gps-id=pcDetailBottomMoreOtherSeller&scm=1007.14976.147898.0&scm_id=1007.14976.147898.0&scm-url=1007.14976.147898.0&pvid=88a589de-ac47-465f-8bea-131e24796f3a&_t=gps-id:pcDetailBottomMoreOtherSeller,scm-url:1007.14976.147898.0,pvid:88a589de-ac47-465f-8bea-131e24796f3a,tpp_buckets:668%230%23131923%2318_668%23808%234094%23529_668%23888%233325%234'
+  },
+  {
+    name: 'WEIDE повседневные Роскошные мужские часы от известных брендов, Кварцевые спортивные кожаные часы, водонепроницаемые часы-браслет 31 заказа(ов)',
+    rating: '4.6/5',
+    description: 'Тип товара: Кварцевые наручные часыШирина браслета: 22mmСтиль: СпортОсобенности: Полный календарьОсобенности: УдаропрочныйОсобенности: ВодонепроницаемыйМеханизм часов: QUARTZЧехол толщина: 15.3mmМатериал корпуса: СплавФорма корпуса: КруглыйТип материала браслета: КожаГлубина водонепроницаемости: 3 БарМатериал коробки и корпуса: БумагаТип материала окошка циферблата: ХардлексНомер модели: UV1609B-4C',
+    image: 'https://ae01.alicdn.com/kf/HTB19Trha2NNTKJjSspcq6z4KVXa5/WEIDE.jpg_50x50.jpg',
+    ref: 'https://aliexpress.ru/item/32831757464.html?spm=a2g0o.detail.1000060.2.2e843a8eRqgN64&gps-id=pcDetailBottomMoreThisSeller&scm=1007.14977.129071.0&scm_id=1007.14977.129071.0&scm-url=1007.14977.129071.0&pvid=d990e758-6f63-4d2e-8fe2-bf4e8ac3b426&_t=gps-id:pcDetailBottomMoreThisSeller,scm-url:1007.14977.129071.0,pvid:d990e758-6f63-4d2e-8fe2-bf4e8ac3b426,tpp_buckets:668%230%23131923%2318_668%23808%234094%23529_668%23888%233325%234'
+  },
+  {
+    name: 'Магнитный кабель 3а usb type-C для samsung S10 S9 Plus, кабель для быстрой зарядки и передачи данных для Xiaomi Redmi Note 10 8 Pro, магнитный кабель type-C',
+    rating:'5.0/5',
+    description: 'Бренд: TwitchМатериал оплётки: НейлонРазъём A: USB Type-AМаксимальный Ток: 3AРазъём B: USB Type-CВ розничной упаковке: ДаОсобенности: МагнитныйОсобенности: Со светодиодным индикаторомОсобенности: Литые разъёмыFeature 1: Magnetic Cable Type C / USB C CableFeature 2: Type-C Cable / Magnetic Cable USB CFeature 3: USB Type C Cable / USB-C CableFeature 4: USB Type C / Magnet Cable / Magnetic Charger / Magnet ChargerCompatible Model 1: For Huawei P20 Lite P30 Pro Mate 20 30 10 CableCompatible Model 2: For Samsung Note 8 9 10 S8 S9 S10 Plus CableCompatible Model 3: For Xiaomi Redmi Note 10 Note 7 Note 8 Pro Type C CableCompatible Model 4: For Oneplus 6T 7T 6 7 Pro Type C Cable',
+    image:'https://ae01.alicdn.com/kf/H00d7008872b0489786909ab86fbef918w/3-usb-type-C-samsung-S10-S9-Plus.jpg_50x50.jpg',
+    ref: 'https://aliexpress.ru/item/4000533693042.html?spm=a2g0o.detail.100009.6.6b4316c8ClSUes&gps-id=pcDetailLeftTopSell&scm=1007.13482.95643.0&scm_id=1007.13482.95643.0&scm-url=1007.13482.95643.0&pvid=44a3e79c-f9bc-4a81-a65c-75a5d5335879&_t=gps-id:pcDetailLeftTopSell,scm-url:1007.13482.95643.0,pvid:44a3e79c-f9bc-4a81-a65c-75a5d5335879,tpp_buckets:668%230%23131923%2318_668%23808%234094%23529_668%23888%233325%234 '
+  },
+  {
+    name: '10 шт./лот 8 мм отверстие: 1,2 мм, Нефритовый белый двойной цветной эффект смешанные стеклянные бусины с распылением окрашенной круглой формы для изготовления ювелирных изделий',
+    description: 'Круглый Стекло  Краски прозрачный двойной Цвет Прядей Размеры: Размеры: около 8 мм в диаметре, отверстие: 1,2 мм; Кол-во: 100 шт./пакет; Применение случаи жизни  : Его можно сочетать с крючками, прыжками и булавками, чтобы быть серьгами, браслетами и так далее.  ',
+    image: 'https://ae01.alicdn.com/kf/HTB1O1vFXJzvK1RkSnfoq6zMwVXaq/10-8-1-2.jpg_50x50.jpg',
+    ref: 'https://aliexpress.ru/item/32956763225.html?spm=a2g0o.detail.1000014.29.5f03e002F0nPGk&gps-id=pcDetailBottomMoreOtherSeller&scm=1007.14976.147898.0&scm_id=1007.14976.147898.0&scm-url=1007.14976.147898.0&pvid=5ff099b6-d2d2-406b-b9ee-c4a3f8b0507d&_t=gps-id:pcDetailBottomMoreOtherSeller,scm-url:1007.14976.147898.0,pvid:5ff099b6-d2d2-406b-b9ee-c4a3f8b0507d,tpp_buckets:668%230%23131923%2318_668%23808%234094%23529_668%23888%233325%234'
+  },
+  {
+    name:'Магнитный usb-кабель 3А, Micro USB type-C кабель для быстрой зарядки samsung, кабель для синхронизации данных, для iphone 11 XS XR, Магнитный зарядный USB-C',
+    rating:'5.0/5',
+    description: 'Бренд: TwitchМатериал оплётки: НейлонРазъём A: USB Type-AМаксимальный Ток: 3AРазъём B: USB Type-CРазъём B: Micro-USBВ розничной упаковке: ДаОсобенности: МагнитныйОсобенности: 3-в-1Особенности: Со светодиодным индикаторомОсобенности: Литые разъёмыFeature 1: Magnetic Cable / USB Cable / Mobile Phone Cable / KabelFeature 2: Magnetic USB Charging Cable / Magnetic Charging CableFeature 3: 3A Fast Magnetic Cable Data Cable Cargador Magnetico LEDFeature 4: Magnetic Cable Micro USB / Microusb / Android CableFeature 5: Magnetic Cable Type C / USB C / USB Type C CableFeature 6: For iphone Magnetic Cable / USB Cable For iphone Magnet CableCompatible Phone 1: For iphone 7 8 Plus Cable For iphone 11 Pro XR X XS MaxCompatible Phone 2: For Samsung S10 S9 S8 Plus A50 A70 USB Type C Cable Fast ChargingCompatible Phone 3: For Huawei P30 P20 Pro Mate 20 Mate 30 Lite Oneplus 6T 7TCompatible Phone 4: Cable For Xiaomi Redmi Note 10 Note 7 Note 8 ProCompatible Phone 5: For Nokia 8 LG G5 G6 Sony Phone Cable',
+    image:'https://ae01.alicdn.com/kf/H7e5ca038e3614e45a53da67deb993d48l/usb-3-Micro-USB-type-C.jpg_50x50.jpg',
+    ref:'https://aliexpress.ru/item/4000527624853.html?spm=a2g0o.detail.100009.4.5f03e002qfzL75&gps-id=pcDetailLeftTopSell&scm=1007.13482.95643.0&scm_id=1007.13482.95643.0&scm-url=1007.13482.95643.0&pvid=80aed497-3a53-4aa4-82e4-b81a75bde684&_t=gps-id:pcDetailLeftTopSell,scm-url:1007.13482.95643.0,pvid:80aed497-3a53-4aa4-82e4-b81a75bde684,tpp_buckets:668%230%23131923%2318_668%23808%234094%23529_668%23888%233325%234'
+  },
+  {
+    name:'Norns Спорт бег музыка наушники портативный наушники Проводная гарнитура с микрофоном телефон Android',
+    rating: '5.0/5',
+    description:'Статус продукта: в наличии Стиль: вкладыши Функция: супер бас-гитара Частота: 20-20,000 Гц  Чувствительность: 106dB Сопротивление: 16 Oh Длина кабеля: 1,2 м Штекер: 3,5 мм разъем Совместимость: смартфоны, планшеты, mp3-плееры или другие устройства, совместимые с 3,5 мм аудио кабелем порт, идеально подходит для iphone 4 5 6, для samsung и т. д. смарт-наушники',
+    image:'https://ae01.alicdn.com/kf/H4fbb3cbe9695420cae3f13808bb7339eS/Norns.jpg_50x50.jpg',
+    ref:'https://aliexpress.ru/item/4000538980297.html?spm=a2g0o.detail.1000014.19.604a6cc3D8TnfC&gps-id=pcDetailBottomMoreOtherSeller&scm=1007.14976.147898.0&scm_id=1007.14976.147898.0&scm-url=1007.14976.147898.0&pvid=c21806ce-2c09-4c4a-a2e9-e7bcfd9efde5&_t=gps-id:pcDetailBottomMoreOtherSeller,scm-url:1007.14976.147898.0,pvid:c21806ce-2c09-4c4a-a2e9-e7bcfd9efde5,tpp_buckets:668%230%23131923%2318_668%23808%234094%23529_668%23888%233325%234'
+  },
+  {
+    name:'Norns Проводные Bluetooth наушники спортивные для бега музыкальные наушники женские милые наушники гарнитура с микрофоном телефон Android IOS',
+    rating: '4.0/5',
+    description:'Бренд: NoEnName_NullКлавиши управления: ДаАктивное шумоподавление: ДаКонструкция: С заушинойТип беспроводной связи: BluetoothРазъёмы: USBСпособ подключения: КабельноеТип излучателей: ДинамическиеРегулировка громкости: ДаДиапазон воспроизводимых частот: 20-20000 ГцНазначение: УниверсальныеНазначение: Гарнитура для телефонаНазначение: Для iPodНазначение: Hi-FiНазначение: СпортивныеЧувствительность: 116±3 дБПодключение: ДаФорма коннектора: wirelessПоддерживаемые аудиокодеки: Apt-XПоддержка карт памяти: НетС микрофоном: ДаНомер модели: ej-003Сопротивление: 32ΩЗащита от влаги: НетДлина кабеля: 0.6 мУправление через приложение: ДаCompatible iPhone Series Phone: For iPhone X, iPhone 8, iPhone 7, iPhone 6s, iPhone SE, iphone 5,Compatible Samsung Series Phone: For Galaxy S, Galaxy Note, Galaxy C, Galaxy OnCompatible with Android Phone: For Xiaomi, Huawei, LG, vivo, OPPO, HTC, AGM, nubia, OnePlus, NokiaCompatible with Android Phones: For ZTE, Honor, Meizu, Moto, Philips, Sony, Coolpad, Meitu, SmartisanCompatible: For PC, iPad, PSP and so on enable Windows BluetoothColour: Black, Red, Blue, Yellow, GreenSweatproof: YESBluetooth Version: V4.1+EDRSupports: HSP, HFP, A2DP and AVRCP/HandfreeMicrophone: Built-InOperation distance: Up To 10MCharging time: about 1 hourPlaying Time: about 3-4 hoursCall time: about 4 hoursStandby time: Up To 100 hoursSound Quality: Non-Stereo SoundIncluding: Earphone *1, USB charging cable *1, manual *1, Ear cap (S,M,L) *3pairs',
+    image:'https://ae01.alicdn.com/kf/H7104d3120fcf4a689f34dcafc3897916m/Norns-Bluetooth.jpg_50x50.jpg',
+    ref:'https://aliexpress.ru/item/4000528707435.html?spm=a2g0o.detail.1000060.3.37ec6275bR4oVD&gps-id=pcDetailBottomMoreThisSeller&scm=1007.14977.129071.0&scm_id=1007.14977.129071.0&scm-url=1007.14977.129071.0&pvid=6073329f-9eff-4be7-b05d-8a13ad69ba7d&_t=gps-id:pcDetailBottomMoreThisSeller,scm-url:1007.14977.129071.0,pvid:6073329f-9eff-4be7-b05d-8a13ad69ba7d,tpp_buckets:668%230%23131923%2318_668%23808%234094%23529_668%23888%233325%234'
+  },
+  {
+    name:'Norns Спорт бег музыка наушники портативный наушники Проводная гарнитура с микрофоном телефон Android',
+    rating:'4.7/5',
+    description:'Бренд: NoEnName_NullКлавиши управления: ДаАктивное шумоподавление: ДаКонструкция: ВкладышиТип беспроводной связи: НетРазъёмы: НетСпособ подключения: КабельноеТип излучателей: ДинамическиеРегулировка громкости: ДаДиапазон воспроизводимых частот: 20-20 ГцНазначение: МониторныеНазначение: УниверсальныеНазначение: Гарнитура для телефонаНазначение: Для iPodНазначение: СпортивныеЧувствительность: 110 дБПодключение: НетФорма коннектора: ПрямойПоддерживаемые аудиокодеки: Apt-XПоддержка карт памяти: НетС микрофоном: ДаНомер модели: ej-006Сопротивление: 32ΩЗащита от влаги: НетДлина кабеля: 1.0 мУправление через приложение: НетFeature 1: For sumsung s8 earphoneFeature 2: Most of Smartphone/PC/Pad/LaptopFeature 3: For Samsung Galaxy S6 SMG920Feature 4: High quality earphone',
+    image: 'https://ae01.alicdn.com/kf/Hc06b52398e63416f91be62ff5a5a92b6X/Norns.jpg_50x50.jpg',
+    ref: 'https://aliexpress.ru/item/4000529335942.html?spm=a2g0o.detail.1000060.2.2d2014637BlBVS&gps-id=pcDetailBottomMoreThisSeller&scm=1007.14977.129071.0&scm_id=1007.14977.129071.0&scm-url=1007.14977.129071.0&pvid=4c085a29-925c-4054-802d-8ee1daba3025&_t=gps-id:pcDetailBottomMoreThisSeller,scm-url:1007.14977.129071.0,pvid:4c085a29-925c-4054-802d-8ee1daba3025,tpp_buckets:668%230%23131923%2318_668%23808%234094%23529_668%23888%233325%234'
+  }
+]; 
+
+
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
